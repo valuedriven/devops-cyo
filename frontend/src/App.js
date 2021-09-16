9d81cc2074c3f496b067fcc2f037812e3027fa2b
valuedriven <junilson@hotmail.com>
2021-09-16 18:51:53 -0300

    Configuração do eslint e prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          Devops-CYO
+        Devops-CYO
       </header>
     </div>
   );
