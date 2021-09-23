178d7878494b56196f15d64efaf1c5038593033e
valuedriven <junilson@hotmail.com>
2021-09-23 11:00:01 -0300

    Alterações no branch POC

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          Devops-CYO
-      </header>
+    <div className="App">      
+      <h1> Devops-CYO </h1>
+      <h2> Prova de conceito de uma nova tecnologia</h2>
+      <h3> v1.1.0 </h3>
     </div>
   );
 }
