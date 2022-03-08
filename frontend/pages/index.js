a8bb5bbcff90b69d06c89b5674fe98190c76f999
valuedriven <junilson@hotmail.com>
2022-03-08 18:12:34 -0300

    Configuração do módulo frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Cloud Native App</title>
+        <meta name="description" content="Example of DevOps App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p />
+        <h1>Welcome to DevOps-Cyo!</h1>
+      </main>
+    </div>
+  );
+}
